b7d53a5cdc8a77564aaa9408a4ba78b75edbf999
Amal-uw <uwgating@gmail.com>
2024-12-25 19:08:36 +0600

    Update Gift.java

--- src/main/java/gift/Gift.java
+++ src/main/java/gift/Gift.java
@@ -2,6 +2,6 @@ package gift;
 
 public class Gift {
     public static void main(String[] args) {
-        System.out.println("Gift for everyone");
+        System.out.println("Gift for everyone, but not fot u urod!");
     }
 }
